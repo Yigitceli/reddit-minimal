0902e2e512ee6fe63c5dc72b71c3ee89e47be1cd
Yigitceli <Yigit.celi@outlook.com>
2021-08-12 18:18:13 +0300

    Logo set url to 'popular'

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import SearchBar from "../SearchBar/SearchBar";
+import { setUrl } from "../../store/RedditData";
 import "./Nav.css";
 
 export default function Nav() {
+  const dispatch = useDispatch();
+  const clickHandler = () => {
+    dispatch(setUrl('popular'));
+  };
   return (
     <div id="header">
-      <div className="logo">
+      <div onClick={clickHandler} className="logo">
         <i class="fab fa-reddit"></i>
         <p>
           <span style={{ color: "#536DF3" }}>Reddit</span>Minimal
         </p>
       </div>
-      <SearchBar/>
+      <SearchBar />
     </div>
   );
 }
--- src/store/RedditData.js
+++ src/store/RedditData.js
@@ -22,16 +22,11 @@ export const DataSlice = createSlice({
         setSubReddits: (state, action) => {
             state.subReddits = action.payload;
         },
-        addComments: (state, action) => {
-            const postToAdd = state.data.find(item => {return item.data.id === action.payload.id});
-            const newData = state.data.filter(item => {return item.data.id !== action.payload.id});
-            state.data = [...newData, {...postToAdd, comments:[action.payload.comments]}];
-
-        }
+        
         
         
     }
 });
 
-export const {toggleLoading, setData, setUrl, setSubReddits, addComments} = DataSlice.actions;
+export const {toggleLoading, setData, setUrl, setSubReddits} = DataSlice.actions;
 export default DataSlice.reducer; 
